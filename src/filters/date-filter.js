d77bf83c56f71fb4b0240b72cc4fb8de19e82bfe
Dana Byerly <dana@superterrific.org>
2021-04-01 20:24:50 -0400

    Luxon, RSS update & a post

--- src/filters/date-filter.js
+++
@@ -1,6 +0,0 @@
-const moment = require('moment');
-
-module.exports = value => {
-  const dateObject = moment.utc(value);
-  return `${dateObject.format('MMMM')} ${dateObject.format('Do')}, ${dateObject.format('YYYY')}`;
-};
